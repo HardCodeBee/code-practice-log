894b68fd26c0dccaf4dfe8137ba38c72863600c3
Shengjie Liu <u7830488@anu.edu.au>
2025-06-27 12:30:53 +1000

    finish Leetcode Q5 (dynamic programing solution)

--- leetcode/6.22-6.29/Q5_Longest_Palindromic_Substring.java
+++ leetcode/6.22-6.29/Q5_Longest_Palindromic_Substring.java
@@ -23,7 +23,7 @@ public class Q5_Longest_Palindromic_Substring {
 
 
 
-    public String longestPalindrome(String s) {
+    public String longestPalindrome_1(String s) {
         /**Brute Force + Set Approach*
          *  Time Complexity: O(n²)
          *  Space Complexity: O(k)
@@ -61,5 +61,42 @@ public class Q5_Longest_Palindromic_Substring {
             right--;
         }
         return true;
+    }
+
+
+    public String longestPalindrome_2(String s) {
+        /**
+         * Dynamic Programming
+         * For substrings of length 1:
+         * They are trivially palindromes.
+         *
+         * For substrings of length 2:
+         * They are palindromes if and only if the two characters are equal.
+         *
+         * For substrings of length >2:
+         * P(i, j) = P(i + 1, j - 1) && (s[i] == s[j])
+         */
+
+        int ans1 = 0, ans2 = 0;
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j) {
+                    dp[j] = true;
+                } else if (j + 1 == i) {
+                    dp[j] = s.charAt(j) == s.charAt(i);
+                } else {
+                    // dp[j] only depends on dp[j + 1] from the previous round of the outer loop
+                    // (when the right boundary was at i - 1).
+                    dp[j] = s.charAt(j) == s.charAt(i) ? dp[j + 1] : false;
+                }
+                if (dp[j] && i - j > ans2 - ans1) {
+                    //update result if substring is valid and longer
+                    ans2 = i;
+                    ans1 = j;
+                }
+            }
+        }
+        return s.substring(ans1, ans2 + 1);
     }
 }
